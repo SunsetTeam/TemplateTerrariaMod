be19bd48c8b8872ecb8236adee3155ba10b1a029
KirillAldashkin <a89086520092k@yandex.ru>
2021-08-09 12:04:24 +0800

    Project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Crymle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Crymle")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c989142a-60ce-4cb4-ad70-50c10a0fecf0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TemplateMod.cs
@@ -0,0 +1,18 @@
+﻿using Terraria.ModLoader;
+
+namespace TemplateTerrariaMod
+{
+    public class TemplateMod : Mod
+    {
+        public TemplateMod()
+        {
+            Properties = new ModProperties()
+            {
+                Autoload = true,
+                AutoloadBackgrounds = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+    }
+}
